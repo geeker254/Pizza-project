83700e68350ac977a5cfed037ca92aa7911670ee
geeker254 <eugenebiwott@gmail.com>
2020-08-04 07:12:16 +0300

    Total calculation

--- script.js
+++ script.js
@@ -1,110 +1,76 @@
+var input = document.getElementById("myInput");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("myBtn").click();
+  }
+});
+
 //In the javascript code
-var theForm = document.forms["cakeform"];
+var theForm = document.forms["pizzaform"];
 
 var cake_prices = new Array();
-cake_prices["Round6"]=20;
-cake_prices["Round8"]=25;
-cake_prices["Round10"]=35;
-cake_prices["Round12"]=75;
+pizza_prices["Round6"]=20;
+pizza_prices["Round8"]=25;
+pizza_prices["Round10"]=35;
 
-// getCakeSizePrice() finds the price based on the size of the cake.
-// Here, we need to take user's the selection from radio button selection
-function getCakeSizePrice()
+function getpizzasizePrice()
 {
-    var cakeSizePrice=0;
-    //Get a reference to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the cake the user Chooses name=selectedCake":
-    var selectedCake = theForm.elements["selectedcake"];
-    //Here since there are 4 radio buttons selectedCake.length = 4
-    //We loop through each radio buttons
-    for(var i = 0; i < selectedCake.length; i++)
+    var pizzaSizePrice=0;
+    var theForm = document.forms["pizzaform"]
+    var selectedPizza = theForm.elements["pizza_prices"]
+    for(var i = 0; i < selectedPizza.length; i++)
     {
-        //if the radio button is checked
-        if(selectedCake[i].checked)
+        if(selectedPizza[i].checked)
         {
-            //we set cakeSizePrice to the value of the selected radio button
-            //i.e. if the user choose the 8" cake we set it to 25
-            //by using the cake_prices array
-            //We get the selected Items value
-            //For example cake_prices["Round8".value]"
-            cakeSizePrice = cake_prices[selectedCake[i].value];
+            pizzaSizePrice = pizza_prices[selectedPizza[i].value];
             //If we get a match then we break out of this loop
             //No reason to continue if we get a match
             break;
         }
     }
-    //We return the cakeSizePrice
     return cakeSizePrice;
 }
 
-var filling_prices= new Array();
-filling_prices["None"]=0;
-filling_prices["Lemon"]=5;
-filling_prices["Custard"]=5;
-filling_prices["Fudge"]=7;
-filling_prices["Mocha"]=8;
-filling_prices["Raspberry"]=10;
+var pizza_type= new Array();
+filling_prices["Peperoni"]=5;
+filling_prices["Mozzarela"]=5;
+filling_prices["Hawaii"]=7;
+filling_prices["Pineaple"]=8;
+filling_prices["Corn"]=10;
 
-//This function finds the filling price based on the
-//drop down selection
+
 function getFillingPrice()
 {
-    var cakeFillingPrice=0;
-    //Get a reference to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the select id="filling"
-     var selectedFilling = theForm.elements["filling"];
+    var pizzaFillingPrice=0;
+    var theForm = document.forms["pizzaform"]
+     var selectedFilling = theForm.elements["pizza_type"];
 
-    //set cakeFilling Price equal to value user chose
-    //For example filling_prices["Lemon".value] would be equal to 5
-    cakeFillingPrice = filling_prices[selectedFilling.value];
+    pizzaFillingPrice = filling_prices[selectedFilling.value];
 
-    //finally we return cakeFillingPrice
-    return cakeFillingPrice;
+    return pizzaFillingPrice;
 }
 
-//candlesPrice() finds the candles price based on a check box selection
-function candlesPrice()
+function saucePrice()
 {
-    var candlePrice=0;
-    //Get a reference to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the checkbox id="includecandles"
-    var includeCandles = theForm.elements["includecandles"];
+    var saucePrice=0;
+    var theForm = document.forms["pizzaform"];
+    var includeCandles = theForm.elements["includesauce"];
 
-    //If they checked the box set candlePrice to 5
     if(includeCandles.checked==true)
     {
         candlePrice=5;
     }
-    //finally we return the candlePrice
     return candlePrice;
 }
 
-{
-    //Assume form with id="theform"
-    var theForm = document.forms["cakeform"];
-    //Get a reference to the TextBox
-    var quantity = theForm.elements["quantity"];
-    var howmany =0;
-    //If the textbox is not blank
-    if(quantity.value!="")
-    {
-        howmany = parseInt(quantity.value);
-    }
-return howmany;
-}
 
 function getTotal()
 {
-    //Here we get the total price by calling our function
-    //Each function returns a number so by calling them we add the values they return together
-    var cakePrice = getCakeSizePrice() + getFillingPrice() +
-                          candlesPrice() + insciptionPrice();
-
+    var pizzaPrice = getpizzasizePrice() + getFillingPrice() +
+            saucePrice();
     //display the result
     document.getElementById('totalPrice').innerHTML =
-                                      "Total Price For Cake $"+cakePrice;
+                                      "Total Price For Pizza $"+ pizzaPrice;
 
 }
